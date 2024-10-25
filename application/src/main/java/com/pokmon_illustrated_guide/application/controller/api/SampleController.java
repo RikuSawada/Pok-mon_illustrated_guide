448b9f3b698da5a48188bb2d5cbeec5a9ff5f7b3
Riku Sawada <rikusawada@RikunoMacBook-Pro.local>
2024-10-25 21:39:47 +0900

    ロジックを一箇所にまとめる

--- application/src/main/java/com/pokmon_illustrated_guide/application/controller/api/SampleController.java
+++ application/src/main/java/com/pokmon_illustrated_guide/application/controller/api/SampleController.java
@@ -2,8 +2,19 @@ package com.pokmon_illustrated_guide.application.controller.api;
 
 import com.pokmon_illustrated_guide.application.controller.api.params.Sample.SampleRequestParams;
 import com.pokmon_illustrated_guide.application.controller.api.params.Sample.SampleResponseParams;
+import com.pokmon_illustrated_guide.application.domain.entity.PokemonInfo;
 import com.pokmon_illustrated_guide.application.domain.service.ISampleService;
 import jakarta.validation.Valid;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,7 +36,34 @@ public class SampleController {
   @GetMapping("translate")
   public SampleResponseParams translate(@Valid @ModelAttribute SampleRequestParams params) {
     logger.info("/api/sample/translate:"+params);
-    return new SampleResponseParams(sampleService.translate());
+
+    String url = "https://pokeapi.co/api/v2/pokemon/";
+
+    // http通信を行うための準備
+    HttpClient client = HttpClient.newHttpClient();
+    HttpRequest request = HttpRequest.newBuilder()
+        .uri(URI.create(url))
+        .build();
+
+    List<PokemonInfo> response = new ArrayList<PokemonInfo>();
+
+    try{
+      // ポケモンAPIにリクエスト送信
+      HttpResponse<String> result = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+      // 受け取ったレスポンスをJSONをパースしてポケモンの名前を取得・表示
+      JSONObject jsonObject = new JSONObject(result.body());
+      JSONArray results = jsonObject.getJSONArray("results");
+
+      for (int i=0;i<results.length();i++){
+        JSONObject pokemon = results.getJSONObject(i);
+        response.add(new PokemonInfo(pokemon.getString("name") ,pokemon.getString("url")));
+      }
+    } catch (IOException | InterruptedException | JSONException e) {
+      throw new RuntimeException(e);
+    }
+
+    return new SampleResponseParams(response);
   }
 
   @PostMapping("translate")
